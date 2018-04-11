d745e1cf2429028109fec7dc2a15afff20dd7747
刘鹏 <liupeng@liupengdeMacBook-Pro.local>
2018-04-11 17:38:13 +0800

    Initial commit

---
+++ lib/loss/loss.py
@@ -0,0 +1,125 @@
+# coding=utf-8
+
+import tensorflow as tf 
+import numpy as np 
+
+
+# quared loss
+def squared_loss(label, logit):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(label - logit), 1))
+    return loss
+
+# sigmoid loss
+def sigmoid_loss(label, logit):
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = label, logits = logit))
+    return loss
+
+# softmax loss
+def softmax_loss(label, logit):
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = label, logits = logit))
+    return loss
+
+# triplet loss 
+def triplet_loss(anchor, positive, negative, alpha):
+    # 理论可以看这里： https://blog.csdn.net/tangwei2014/article/details/46788025
+    # facenet理解： http://www.mamicode.com/info-detail-2096766.html
+    """Calculate the triplet loss according to the FaceNet paper
+    
+    Args:
+      anchor: the embeddings for the anchor images.
+      positive: the embeddings for the positive images.
+      negative: the embeddings for the negative images.
+  
+    Returns:
+      the triplet loss according to the FaceNet paper as a float tensor.
+    """
+    with tf.variable_scope('triplet_loss'):
+        pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), 1)
+        neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), 1)
+        
+        basic_loss = tf.add(tf.subtract(pos_dist,neg_dist), alpha)
+        loss = tf.reduce_mean(tf.maximum(basic_loss, 0.0), 0)
+      
+    return loss
+
+
+# center loss
+def center_loss(features, label, alfa, nrof_classes):
+    """Center loss based on the paper "A Discriminative Feature Learning Approach for Deep Face Recognition"
+       (http://ydwen.github.io/papers/WenECCV16.pdf)
+    """
+    """获取center loss及center的更新op 
+    还可参考博客： https://blog.csdn.net/u014365862/article/details/79184966     
+    Arguments: 
+        features: Tensor,表征样本特征,一般使用某个fc层的输出,shape应该为[batch_size, feature_length]. 
+        labels: Tensor,表征样本label,非one-hot编码,shape应为[batch_size]. 
+        alpha: 0-1之间的数字,控制样本类别中心的学习率,细节参考原文. 
+        num_classes: 整数,表明总共有多少个类别,网络分类输出有多少个神经元这里就取多少. 
+     
+    Return： 
+        loss: Tensor,可与softmax loss相加作为总的loss进行优化. 
+        centers: Tensor,存储样本中心值的Tensor，仅查看样本中心存储的具体数值时有用. 
+        centers_update_op: op,用于更新样本中心的op，在训练时需要同时运行该op，否则样本中心不会更新 
+    """  
+    nrof_features = features.get_shape()[1]
+    centers = tf.get_variable('centers', [nrof_classes, nrof_features], dtype=tf.float32,
+        initializer=tf.constant_initializer(0), trainable=False)
+    label = tf.reshape(label, [-1])
+    centers_batch = tf.gather(centers, label)
+    diff = (1 - alfa) * (centers_batch - features)
+    centers = tf.scatter_sub(centers, label, diff)
+    loss = tf.reduce_mean(tf.square(features - centers_batch))
+    return loss, centers
+
+
+def get_center_loss(features, labels, alpha, num_classes):  
+    """获取center loss及center的更新op 
+     
+    Arguments: 
+        features: Tensor,表征样本特征,一般使用某个fc层的输出,shape应该为[batch_size, feature_length]. 
+        labels: Tensor,表征样本label,非one-hot编码,shape应为[batch_size]. 
+        alpha: 0-1之间的数字,控制样本类别中心的学习率,细节参考原文. 
+        num_classes: 整数,表明总共有多少个类别,网络分类输出有多少个神经元这里就取多少. 
+     
+    Return： 
+        loss: Tensor,可与softmax loss相加作为总的loss进行优化. 
+        centers: Tensor,存储样本中心值的Tensor，仅查看样本中心存储的具体数值时有用. 
+        centers_update_op: op,用于更新样本中心的op，在训练时需要同时运行该op，否则样本中心不会更新 
+    """  
+    # 获取特征的维数，例如256维  
+    len_features = features.get_shape()[1]  
+    # 建立一个Variable,shape为[num_classes, len_features]，用于存储整个网络的样本中心，  
+    # 设置trainable=False是因为样本中心不是由梯度进行更新的  
+    centers = tf.get_variable('centers', [num_classes, len_features], dtype=tf.float32,  
+        initializer=tf.constant_initializer(0), trainable=False)  
+    # 将label展开为一维的，输入如果已经是一维的，则该动作其实无必要  
+    # labels = tf.reshape(labels, [-1])
+    labels = tf.argmax(labels, 1)
+      
+    # 根据样本label,获取mini-batch中每一个样本对应的中心值  
+    centers_batch = tf.gather(centers, labels)  
+    # 计算loss  
+    loss = tf.nn.l2_loss(features - centers_batch)  
+      
+    # 当前mini-batch的特征值与它们对应的中心值之间的差  
+    diff = centers_batch - features  
+      
+    # 获取mini-batch中同一类别样本出现的次数,了解原理请参考原文公式(4)  
+    unique_label, unique_idx, unique_count = tf.unique_with_counts(labels)  
+    appear_times = tf.gather(unique_count, unique_idx)  
+    appear_times = tf.reshape(appear_times, [-1, 1])  
+      
+    diff = diff / tf.cast((1 + appear_times), tf.float32)  
+    diff = alpha * diff  
+      
+    centers_update_op = tf.scatter_sub(centers, labels, diff)  
+      
+    return loss, centers, centers_update_op 
+
+
+# 加入L2正则化的loss
+def add_l2(loss, weight_decay):
+    l2_losses = [weight_decay * tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'weights' in v.name]
+    reduced_loss = tf.reduce_mean(loss) + tf.add_n(l2_losses)
+    return reduced_loss
+
---
+++ test_accuracy.py
@@ -0,0 +1,56 @@
+# coding = utf-8
+import tensorflow as tf 
+from tensorflow.python.framework import graph_util
+from lib.data_load.data_load import load_image
+from lib.utils.utils import input_placeholder3, g_parameter, data_norm, model_accuracy
+from lib.utils.utils import get_next_batch_from_path3, shuffle_train_data, cost, build_net3
+from keras.utils import np_utils
+import cv2
+import numpy as np
+import os
+import sys
+import config
+
+num_classes = config.num_classes
+height, width = config.height, config.width
+arch_model = config.arch_model
+batch_size = config.batch_size
+sample_dir = 'gender'
+train_rate = 1.0
+test_data, test_label, valid_data, valid_label, test_n, valid_n, note_label = load_image(sample_dir, train_rate).gen_train_valid()
+print ('test_n', test_n)
+print ('valid_n', valid_n)
+test_label = np_utils.to_categorical(test_label, num_classes)
+valid_label = np_utils.to_categorical(valid_label, num_classes)
+
+
+X1,X2,X3, Y, is_train, keep_prob_fc = input_placeholder3(height, width, num_classes)
+net, net_vis = build_net3(X1,X2,X3, num_classes, keep_prob_fc, is_train,arch_model)
+loss = cost(Y, net)
+accuracy = model_accuracy(net, Y, num_classes)
+# predict = tf.reshape(net, [-1, num_classes], name='predictions')
+
+
+
+if __name__ == '__main__':
+    train_dir = 'model'
+    latest = tf.train.latest_checkpoint(train_dir)
+    if not latest:
+        print ("No checkpoint to continue from in", train_dir)
+        sys.exit(1)
+    print ("resume", latest)
+    sess = tf.Session()
+    saver = tf.train.Saver(tf.global_variables())
+    saver.restore(sess, latest)
+    test_ls = 0
+    test_acc = 0
+    for batch_i in range(int(test_n/batch_size)):
+            images_test1, images_test2, images_test3, labels_test = get_next_batch_from_path3(test_data, test_label, batch_i, height, width, batch_size=batch_size, training=False)
+            epoch_ls, epoch_acc = sess.run([loss, accuracy], feed_dict={X1: images_test1,X2: images_test2,X3: images_test3, Y: labels_test, keep_prob_fc:1.0, is_train:False})
+            print (epoch_acc)
+            test_ls = test_ls + epoch_ls
+            test_acc = test_acc + epoch_acc
+    print('Epoch: {:>2}: Validation loss: {:>3.5f}, Validation accuracy: {:>3.5f}'.format(0, test_ls/int(test_n/batch_size), test_acc/int(test_n/batch_size)))
+
+
+
