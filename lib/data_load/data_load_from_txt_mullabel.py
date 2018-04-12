ac0429a1c850eef36fe1dbadb65c3398cd3565d7
MachineLP <MachineLP@163.com>
2018-04-12 09:29:41 +0800

    Update data_load_from_txt_mullabel.py

--- lib/data_load/data_load_from_txt_mullabel.py
+++ lib/data_load/data_load_from_txt_mullabel.py
@@ -2,6 +2,8 @@
 """
 Created on 2017 10.17
 @author: liupeng
+wechat: lp9628
+blog: http://blog.csdn.net/u014365862/article/details/78422372
 """
 
 from lib.utils.multi_label_utils import shuffle_train_data
