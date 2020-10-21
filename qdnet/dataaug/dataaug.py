a98ed9e0c4de63575f619f4040f3eec24f1cb02c
MachineLP <MachineLP@163.com>
2020-10-21 11:14:52 +0800

    添加metric_strategy

--- qdnet/dataaug/dataaug.py
+++ qdnet/dataaug/dataaug.py
@@ -44,3 +44,22 @@ def get_transforms(image_size):
     ])
 
     return transforms_train, transforms_val
+
+
+def get_transforms_(image_size):
+
+    transforms_train = albumentations.Compose([
+        albumentations.HorizontalFlip(p=0.5),
+        albumentations.ImageCompression(quality_lower=99, quality_upper=100),
+        albumentations.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=10, border_mode=0, p=0.7),
+        albumentations.Resize(image_size, image_size),
+        albumentations.Cutout(max_h_size=int(image_size * 0.4), max_w_size=int(image_size * 0.4), num_holes=1, p=0.5),
+        albumentations.Normalize()
+    ])
+
+    transforms_val = albumentations.Compose([
+        albumentations.Resize(image_size, image_size),
+        albumentations.Normalize()
+    ])
+
+    return transforms_train, transforms_val
